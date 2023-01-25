4253f46210df59a6fcfc0171bd33f28627efa06b
Yogesh Nikam <yogesh.nikam@ars.de>
2023-01-25 18:09:37 +0100

    Added product-header component and containers

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  cols = 3;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClumnsCountChanges(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+}
