2dfee3cdd0eebbe16d91d5f5562a372966ce93d1
Yogesh Nikam <yogesh.nikam@ars.de>
2023-01-25 17:50:39 +0100

    Added product-header component and containers

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+  styles: [
+  ]
+})
+export class ProductsHeaderComponent implements OnInit {
+  @Output() columsCountChange = new EventEmitter<number>();
+  sort = 'desc';
+  itemsShowCount = 12;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number) : void {
+    this.itemsShowCount = count;
+  }
+
+  onClumnsUpdated(colsNum: number): void {
+    this.columsCountChange.emit(colsNum);
+  }
+}
