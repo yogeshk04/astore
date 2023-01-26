ff236a3908662f8584a01b7f849ee5d4886e9eb4
Yogesh Nikam <yogesh.nikam@ars.de>
2023-01-26 12:51:34 +0100

    Added product-box component

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: 'filters.component.html'
+})
+export class FiltersComponent implements OnInit {
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['shoes', 'sports', 'cloths']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROW_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROW_HEIGHT[this.cols];
+  category: string | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -13,6 +18,10 @@ export class HomeComponent implements OnInit {
 
   onClumnsCountChanges(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROW_HEIGHT[this.cols];
   }
 
+  onShowCategory(newCategory: string): void{
+    this.category = newCategory;
+  }
 }
